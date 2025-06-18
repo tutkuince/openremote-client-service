d666a61f9d11d722b8db6006d111e4b13a8bc887
Tutku Ince <tutku.ince@outlook.com>
2025-06-18 22:08:47 +0300

    initial commit

---
+++ src/main/java/com/tworun/openremoteclientservice/OpenremoteClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tworun.openremoteclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenremoteClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenremoteClientServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tworun/openremoteclientservice/OpenremoteClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tworun.openremoteclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenremoteClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
