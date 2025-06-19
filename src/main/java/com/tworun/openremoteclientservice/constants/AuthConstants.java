f1c30143373d3dfb051bad7951538cb9e474b9c4
Tutku Ince <tutku.ince@outlook.com>
2025-06-19 17:42:27 +0300

    AuthConstants

---
+++ src/main/java/com/tworun/openremoteclientservice/constants/AuthConstants.java
@@ -0,0 +1,11 @@
+package com.tworun.openremoteclientservice.constants;
+
+public final class AuthConstants {
+    private AuthConstants() {}
+
+    public static final String CLIENT_ID_KEY = "client_id";
+    public static final String CLIENT_SECRET_KEY = "client_secret";
+
+    public static final String GRANT_TYPE_CLIENT_CREDENTIALS = "client_credentials";
+    public static final String GRANT_TYPE_KEY = "grant_type";
+}
