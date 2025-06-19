11232dc878cb8db95809f7f2424da7f1ac565699
Tutku Ince <tutku.ince@outlook.com>
2025-06-19 20:18:33 +0300

    AuthException

---
+++ src/main/java/com/tworun/openremoteclientservice/exception/AuthException.java
@@ -0,0 +1,8 @@
+package com.tworun.openremoteclientservice.exception;
+
+public class AuthException extends RuntimeException {
+
+    public AuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
