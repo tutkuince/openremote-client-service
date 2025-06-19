2e0715215186274b5d06b353f8919d8f48fb9cff
Tutku Ince <tutku.ince@outlook.com>
2025-06-19 21:35:02 +0300

    controller

---
+++ src/main/java/com/tworun/openremoteclientservice/controller/AssetController.java
@@ -0,0 +1,27 @@
+package com.tworun.openremoteclientservice.controller;
+
+import com.tworun.openremoteclientservice.dto.AssetCreateRequest;
+import com.tworun.openremoteclientservice.dto.AssetResponse;
+import com.tworun.openremoteclientservice.service.AssetService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assets")
+@RequiredArgsConstructor
+@Slf4j
+public class AssetController {
+
+    private final AssetService assetService;
+
+    @PostMapping
+    public ResponseEntity<AssetResponse> createAsset(@RequestBody AssetCreateRequest request) {
+        return new ResponseEntity<>(assetService.createAsset(request), HttpStatus.CREATED);
+    }
+}
