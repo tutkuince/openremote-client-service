55d9ebeb34b35fff2a6a5c3dd612778b3c6fc6a8
Tutku Ince <tutku.ince@outlook.com>
2025-06-20 18:15:29 +0300

    @AllArgs and @NoArgsConstructs have been added

--- src/main/java/com/tworun/openremoteclientservice/dto/AttributeObject.java
+++ src/main/java/com/tworun/openremoteclientservice/dto/AttributeObject.java
@@ -2,9 +2,13 @@ package com.tworun.openremoteclientservice.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttributeObject {
 
     @NotBlank(message = "Attribute name cannot be blank.")
