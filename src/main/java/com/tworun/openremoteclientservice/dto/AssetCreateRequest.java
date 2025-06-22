aaba68b08cc7e5d31e281466d68f0c87dcd0ad15
Tutku Ince <tutku.ince@outlook.com>
2025-06-22 15:07:34 +0300

    missing fields have been added

--- src/main/java/com/tworun/openremoteclientservice/dto/AssetCreateRequest.java
+++ src/main/java/com/tworun/openremoteclientservice/dto/AssetCreateRequest.java
@@ -1,21 +1,23 @@
 package com.tworun.openremoteclientservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
+import java.util.List;
 import java.util.Map;
 
 @Data
 public class AssetCreateRequest {
+    @Min(value = 0, message = "Version must be 0 or greater")
+    private Long version;
+
     @NotBlank(message = "Asset name cannot be blank.")
     @Size(max = 1023, message = "Name must be at most 1023 characters.")
     private String name;
 
-    @NotBlank(message = "Asset type cannot be blank.")
-    private String type;
+    private Boolean accessPublicRead;
 
     @Pattern(regexp = "^[0-9A-Za-z]{22}$", message = "ParentId must be 22 alphanumeric characters (A-Z, a-z, 0-9).")
     private String parentId;
@@ -24,6 +26,12 @@ public class AssetCreateRequest {
     @Size(max = 255, message = "realm must be at most 255 characters.")
     private String realm;
 
-    @Valid
-    private Map<String, AttributeObject> attributes;
+    @NotBlank(message = "Asset type cannot be blank.")
+    private String type;
+
+    private List<String> path;
+
+    @NotNull(message = "Attributes must not be null")
+    @Size(min = 1, message = "At least one attribute is required")
+    private Map<String, @Valid AttributeObject> attributes;
 }
